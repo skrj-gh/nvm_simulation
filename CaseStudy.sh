815bdd6e6458a512d5f7f3e70ab05e83859ee5a1
Tristan Seidl <tristan.seidl@tu-dortmund.de>
2025-03-25 18:02:36 +0100

    Removed unneeded script.

--- CaseStudy.sh
+++
@@ -1,6 +0,0 @@
-###
-# TODO
-# This script contains one command for each case study,
-# moving the respository into the desired state such that
-# the user can directly start with the case study.
-###
